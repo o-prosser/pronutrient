3e6b993fe9659a822af627a995fc22d848bb4db5
Owen Prosser <owenprosser@icloud.com>
2025-05-26 17:10:17 +0100

    Fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,6 +3,8 @@ import { getSession } from "@/lib/auth";
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await getSession();
 
+  if (!session || !session.user) throw new Error();
+
   return (
     <>
       {session.user.firstName}
