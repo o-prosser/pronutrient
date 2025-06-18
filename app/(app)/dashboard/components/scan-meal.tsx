636f11765f041bd8a42f3ddc3bf3566da565aa0a
Owen Prosser <owenprosser@icloud.com>
2025-06-18 20:28:54 +0100

    Fetch product

--- app/(app)/dashboard/components/scan-meal.tsx
+++ app/(app)/dashboard/components/scan-meal.tsx
@@ -1,11 +1,44 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import BarcodeScanner from "react-qr-barcode-scanner";
-import {Dialog, DialogTrigger, DialogContent, DialogTitle} from '@/components/ui/dialog'
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 const ScanMeal = () => {
-  const [data, setData] = useState("Scanning")
+  const [data, setData] = useState("Scanning");
+  const [barcode, setBarcode] = useState(barcode);
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    if (!barcode) return;
+
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(
+          `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`,
+        );
+        const data = await response.json();
+
+        if (data.status === 1) {
+          setProduct(data.product);
+          setError("");
+        } else {
+          setProduct(null);
+          setError("Product not found.");
+        }
+      } catch (err) {
+        setError("Error fetching product data.");
+        console.error(err);
+      }
+    };
+
+    fetchProduct();
+  }, [barcode]);
 
   return (
     <Dialog>
@@ -22,16 +55,33 @@ const ScanMeal = () => {
           height={500}
           onUpdate={(err, result) => {
             // @ts-expect-error skfddpsknfdkf
-            if (result) setData(result.text);
-            else setData("Not Found");
+            if (result) {
+              setData(result.text);
+              setBarcode(result.text);
+            } else setData("Not Found");
           }}
           onError={(err) => {
-            console.log(err)
-            setData(err.toString())
+            console.log(err);
+            setData(err.toString());
           }}
         />
 
         {data}
+
+        {barcode}
+
+        {product && (
+          <div style={{ marginTop: "20px" }}>
+            <h3>{product.product_name}</h3>
+            <p>Calories: {product.nutriments["energy-kcal"]} kcal</p>
+            <p>Quantity: {product.quantity}</p>
+            <img
+              src={product.image_url}
+              alt={product.product_name}
+              width="150"
+            />
+          </div>
+        )}
       </DialogContent>
     </Dialog>
   );
