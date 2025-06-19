32154033e6ee55d40c1bc2f0f2836e961df4c8be
Owen Prosser <owenprosser@icloud.com>
2025-06-19 15:15:23 +0100

    Fix header spacing 2

--- app/(app)/components/mobile-tab-bar.tsx
+++ app/(app)/components/mobile-tab-bar.tsx
@@ -1,6 +1,6 @@
 const MobileTabBar = () => {
   return (
-    <header className="fixed inset-x-0 top-0 flex h-16 items-center justify-center gap-2 border-b border-indigo-600/20 bg-indigo-50/50 font-semibold text-indigo-800 backdrop-blur-md [&>svg]:h-6 [&>svg]:w-6 md:hidden [@media(display-mode:standalone)]:pt-[env(safe-area-inset-top)] [@media(display-mode:standalone)]:h-[calc(4rem+env(safe-area-inset-top)]">
+    <header className="fixed inset-x-0 top-0 flex h-16 items-center justify-center gap-2 border-b border-indigo-600/20 bg-indigo-50/50 font-semibold text-indigo-800 backdrop-blur-md [&>svg]:h-6 [&>svg]:w-6 md:hidden standalone:pt-header-inset standalone:h-[calc(4rem+var(--spacing-header-inset)]">
       <svg
         viewBox="0 0 24 24"
         fill="none"
